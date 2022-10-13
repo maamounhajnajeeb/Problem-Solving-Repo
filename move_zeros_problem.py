f9aa2ddf991ed2f85de4f014f0e972d7969253b2
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-10-13 07:28:06 +0300

    Create move_zeros_problem.py

---
+++ move_zeros_problem.py
@@ -0,0 +1,11 @@
+def move_zeros(lst):
+    length, counter = len(lst), 0
+    while counter < len(lst):
+        if lst[counter] == 0:
+            lst.pop(counter)
+        else:
+            counter += 1
+    return lst + [0 for _ in range(length-counter)]
+  
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+print(move_zeros([])
