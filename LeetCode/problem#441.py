69d4820b009c625272fc6689e2dfe04176d07b8b
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-10-26 08:38:25 +0300

    Create problem#441.py

---
+++ LeetCode/problem#441.py
@@ -0,0 +1,23 @@
+# You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins.
+# The last row of the staircase may be incomplete.
+# Given the integer n, return the number of complete rows of the staircase you will build.
+
+# Example : Input: n = 5
+#           Output: 2
+
+# Another example : Input: n = 8
+#                   Output: 3
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        start, end = 1, n
+        res = 0
+        while start <= end:
+            mid = (start+end)//2
+            guess = mid*(mid+1)//2
+            if guess > n:
+                end = mid-1
+            elif guess <= n:
+                start = mid+1
+                res = max(mid, res)
+        return res
