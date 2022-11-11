507ebd1d3565dc3bc89a57365de272a9c15646bd
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-11-11 20:08:43 +0200

    Create problem#0876.py

---
+++ LeetCode/problem#0876.py
@@ -0,0 +1,35 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+# If there are two middle nodes, return the second middle node.
+
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+# Input: head = [1,2,3,4,5,6]
+# Output: [4,5,6]
+# Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+## The Solution ##
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        j: int = 0
+        current = head
+        while head != []:
+            try:
+                head = head.next
+            except AttributeError:
+                break
+            j += 1
+        j = j-1
+        i: int = 0
+        while i<j:
+            current = current.next
+            i += 1
+            j -= 1
+        return current
