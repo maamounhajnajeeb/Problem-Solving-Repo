8344b07a2e30df307af8e0e139eddd935334a695
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-11-16 10:46:08 +0300

    Update problem#0021.py

--- LeetCode/problem#0021.py
+++ LeetCode/problem#0021.py
@@ -49,3 +49,21 @@ class Solution:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyList = ListNode()
+        curr = dummyList
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+                curr = curr.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+                curr = curr.next
+        if not list1:
+            curr.next = list2
+        elif not list2:
+            curr.next = list1
+        return dummyList.next
