77626547c6d36c00bca8816c7fa2d6610823ad44
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-11-26 14:29:08 +0300

    Create problem#0061.py

---
+++ LeetCode/problem#0061.py
@@ -0,0 +1,55 @@
+# Given the head of a linked list, rotate the list to the right by k places.
+
+# Example 1:
+
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [4,5,1,2,3]
+
+# Example 2:
+
+# Input: head = [0,1,2], k = 4
+# Output: [2,0,1]
+
+## First Solution ##
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+
+    def __findLength(self, curr: Optional[ListNode]) -> int:
+        length: int = 0
+        while curr.next:
+            length += 1
+            curr = curr.next
+        return length
+
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next: return head
+        curr: Optional[ListNode] = head
+        length: callable[[Optional[ListNode]], int] = self.__findLength(curr)
+        
+        if k == length+1 or k == 0: return head
+
+        while k > length+1:
+            k = k%(length+1)
+        
+        dummyNode: Optional[ListNode] = ListNode()
+
+        pre: Optional[ListNode]
+        pre, curr = head, head.next
+        while length != k:
+            pre, curr = curr, curr.next
+            length -= 1
+        dummyNode.next = curr
+        pre.next = None
+        pointer: Optional[ListNode] = dummyNode
+        while pointer.next:
+            pointer = pointer.next
+        pointer.next = head
+        return dummyNode.next
+
+## Second Solution ##
