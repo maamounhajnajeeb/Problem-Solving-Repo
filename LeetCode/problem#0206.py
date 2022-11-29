87e14b6c4353b1c784ef277b61b6446af34a89a6
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2022-11-29 11:10:55 +0300

    Create problem#0206.py

---
+++ LeetCode/problem#0206.py
@@ -0,0 +1,32 @@
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+
+# Example 2:
+# Input: head = [1,2]
+# Output: [2,1]
+
+# Example 3:
+# Input: head = []
+# Output: []
+
+## The Solution ##
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr: Optional[Node]; pre: Optional[Node]
+        curr, pre = head, None
+        while curr:
+            nxt = curr.next
+            curr.next = pre
+            pre = curr
+            curr = nxt
+        curr, head = None, None
+        return pre
