1ad6ede4772cbf47282fb61d6f92818901fcea3e
Maamoun Haj Najeeb <87011464+maamoun3911@users.noreply.github.com>
2023-06-08 10:01:06 +0300

    Update problem#1351.py

--- LeetCode/problem#1351.py
+++ LeetCode/problem#1351.py
@@ -7,21 +7,25 @@
 
 
 class Solution:
-    def binSearch(self, row):
-        start, end = 0, len(row) - 1
+    
+    def binary_search(self, row: list[int]) -> int:
+        length = len(row)
+        start, end = 0, length-1
         while start <= end:
-            mid = (start + end) // 2
+            mid = (start+end) // 2
             if row[mid] < 0:
-                if mid - 1 >= 0 and row[mid - 1] < 0:
-                    end = mid - 1
+                if mid-1 >= 0 and row[mid-1] < 0:
+                    end = mid-1
                 else:
-                    return len(row) - mid
-            elif row[mid] >= 0:
-                start = mid + 1
-
-    def countNegatives(self, grid: list[list[int]]) -> int:
-        counter = 0
-        for row in grid:
+                    return length-mid
+            else:
+                start = mid+1
+        return 0
+    
+    def countNegatives(self, matrix: list[list[int]]) -> int:
+        count = 0
+        for row in matrix:
             if row[-1] < 0:
-                counter += self.binSearch(row)
-        return counter
+                count += self.binary_search(row)
+        return count
+
